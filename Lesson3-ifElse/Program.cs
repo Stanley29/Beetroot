dbc98b8ff1839693a82a62f4022c4174ffc80fb4
Denis Drobin <denis.drobin2015@gmail.com>
2022-08-03 21:29:50 +0400

    Add Lesson3-ifElse

---
+++ Lesson3-ifElse/Program.cs
@@ -0,0 +1,22 @@
+﻿int x;
+Console.WriteLine("Input X:");
+if (!Int32.TryParse(Console.ReadLine(), out x))
+{
+    Console.WriteLine("Invalid input");
+    return;
+}
+
+int y;
+Console.WriteLine("Input Y:");
+if (!Int32.TryParse(Console.ReadLine(), out y))
+{
+    Console.WriteLine("Invalid input");
+    return;
+}
+
+int sum = 0;
+for (int i = x; i <= y; i++)
+{
+    sum += i;
+}
+Console.WriteLine($"Sum of numbers: {sum}");
