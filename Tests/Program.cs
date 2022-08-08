5f2817f9f4e3b0340ef2e660f1f2e4b5663f6a47
Denis Drobin <denis.drobin2015@gmail.com>
2022-08-08 17:49:12 +0400

    Add Lesson4-Methods

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -29,11 +29,39 @@
 // }
 // Console.WriteLine(sum);
 
-int i = 2;
-int sum = 0;
-do
-{
-    sum += i;
-    i++;
-} while (i < 10);
-Console.WriteLine(sum);
+// int i = 2;
+// int sum = 0;
+// do
+// {
+//     sum += i;
+//     i++;
+// } while (i < 10);
+// Console.WriteLine(sum);
+//
+// static void MyMethod1(string fname) 
+// {
+//     Console.WriteLine(fname + " Refsnes");
+// }
+//
+// MyMethod1("Liam");
+// MyMethod1("Jenny");
+// MyMethod1("Anja");
+
+// static void MyMethod(string country = "Norway") 
+// {
+//     Console.WriteLine(country);
+// }
+//
+// MyMethod("Sweden");
+// MyMethod("India");
+// MyMethod();
+// MyMethod("USA");
+
+// static void MyMethod(string fname, int age) 
+// {
+//     Console.WriteLine(fname + " is " + age);
+// }
+//
+// MyMethod("Liam", 5);
+// MyMethod("Jenny", 8);
+// MyMethod("Anja", 31);
